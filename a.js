ed24b448da9159c8bb926022d1cacd58635c17b2
ruchitadubey <ruchitadubey19@gmail.com>
2021-03-27 08:41:57 +0530

    26 march code

---
+++ a.js
@@ -0,0 +1,147 @@
+console.log("hello world");
+//variable declaration
+var x=5;
+let y=6;
+const z=8;
+console.log(x+y+z);
+//function declaration
+function getmyname()
+{
+    console.log("i am ruchita");
+}
+//function calling
+getmyname();
+//function declare
+function A()
+{
+    console.log("function A");
+
+}
+function B()
+{
+    console.log("this is function B");
+}
+function C()
+{
+    console.log("this is function C");
+}
+function D()
+{
+    console.log("this is function D");
+}
+function E()
+{
+    console.log("this is function E");
+}
+function F()
+{
+    console.log("this is function F");
+}
+function G()
+{
+    console.log("this is function G");
+}
+function H()
+{
+    console.log("this is function H");
+}
+function I()
+{
+    console.log("this is function I");
+}
+function J()
+{
+    console.log("this is function J");
+}
+function K()
+{
+    console.log("this is function k");
+}
+function L()
+{
+    console.log("this is function L");
+}
+function M()
+{
+    console.log("this is function M");
+}
+function N()
+{
+    console.log("this is function N");
+}
+function O()
+{
+    console.log("this is function O");
+}
+function P()
+{
+    console.log("this is function P");
+}
+function Q()
+{
+    console.log("this is function Q");
+}
+function R()
+{
+    console.log("this is function R");
+}
+function S()
+{
+    console.log("this is function S");
+}
+function T()
+{
+    console.log("this is function T");
+}
+function U()
+{
+    console.log("this is function U");
+}
+function V()
+{
+    console.log("this is function V");
+}
+function W()
+{
+    console.log("this is function W");
+}
+function X()
+{
+    console.log("this is function X");
+}
+function Y()
+{
+    console.log("this is function Y");
+}
+function Z()
+{
+    console.log("this is function Z");
+    
+}
+//function calling
+A();
+B();
+C();
+D();
+E();
+F();
+G();
+H();
+I();
+J();
+K();
+L();
+M();
+N();
+O();
+P();
+Q();
+R();
+S();
+T();
+U();
+V();
+W();
+X();
+Y();
+Z();
