040d5a946ec8f3b75605085381e00b67cb85345b
ruchitadubey <ruchitadubey19@gmail.com>
2021-03-30 19:10:26 +0530

    addition in class

---
+++ h.js
@@ -0,0 +1,18 @@
+class Add
+{
+    
+    
+       mySum()
+       {
+           let x=10;
+           let b=20;
+           let z= x+b;
+           console.log("the sum is:-" +z);
+       }
+
+}
+let obj = new Add();
+obj.mySum();
+
+
+
