0850ddc61fe4346001f8b9cc51e703f01b38828a
ruchitadubey <ruchitadubey19@gmail.com>
2021-03-30 19:20:19 +0530

    subtraction in class

---
+++ i.js
@@ -0,0 +1,18 @@
+class Sub
+{
+// function declare
+ mySub()
+ {
+
+    let v= 30;
+    let g= 20;
+    let m= v-g;
+    console.log("subtraction is :-"+ m);
+ }
+
+
+}
+//object
+let obj = new Sub();
+// access by object
+obj.mySub();
