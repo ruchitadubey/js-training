0a7d158bb74e3830db577f8e7781de57bed75863
ruchitadubey <ruchitadubey19@gmail.com>
2021-03-30 09:03:15 +0530

    diff string

---
+++ e.js
@@ -0,0 +1,10 @@
+//variable declaration
+let x=10;
+const y=20;
+// string types 
+//string 1
+console.log("the sum is:-"+(x+y));
+//type 2
+console.log('the sum is:-'+(x+y));
+//type 3
+console.log(`the sum is:-${x+y}`);
