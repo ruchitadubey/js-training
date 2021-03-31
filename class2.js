13938aba84a775585c02738b57cdba345dacff39
ruchitadubey <ruchitadubey19@gmail.com>
2021-03-31 09:00:01 +0530

    arguments

---
+++ class2.js
@@ -0,0 +1,23 @@
+// calss defination
+class ParaMeter {
+    // properties
+    //constructor
+    //methds
+
+    firstpara(n) { //formal argument
+        console.log(`hello ${n} dubey`);
+
+    }
+    secondPara(){
+        console.log("12");
+    }
+
+
+}// class ending
+// object creation
+// let objectname = new classname
+let k = new ParaMeter();
+// method calling
+//object.member
+k.firstpara("ruchita"); //actual argument
+//k.secondPara();
