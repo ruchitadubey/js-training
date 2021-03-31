47dd60a5ded2582865df78318381d55902369c4e
ruchitadubey <ruchitadubey19@gmail.com>
2021-03-31 08:23:16 +0530

    class basics

---
+++ class.js
@@ -0,0 +1,31 @@
+//class defination
+class MyClass
+{
+
+//properties
+
+//constructor
+
+//Method
+
+myFunction1()
+{
+
+    console.log("hello myfunction1");
+
+}
+ myFunction2()
+ {
+
+    console.log("hello myFunction2");
+ }
+
+} // class defination end
+ 
+// create an object
+//let objectname = new classname
+ let x = new MyClass();
+
+ // calling methods using object
+ x.myFunction1();
+ x.myFunction2();
