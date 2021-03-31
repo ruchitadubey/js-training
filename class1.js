54acbe32ebbd2f5c7af502a7b45bc9c48327ae24
ruchitadubey <ruchitadubey19@gmail.com>
2021-03-31 08:33:12 +0530

    this in class

---
+++ class1.js
@@ -0,0 +1,30 @@
+// class defination
+class MyName
+{
+
+//properties
+//constructor
+//method
+
+ myFirstname()
+ {
+     console.log("ruchita")
+     this.myLastname();
+
+ }
+ myLastname()
+ {
+
+    console.log("Dubey");
+ }
+
+
+}// class ending
+
+// creating an object
+//let objectname = new classname
+
+let r = new MyName();
+
+// calling methods using object
+ r.myFirstname();
