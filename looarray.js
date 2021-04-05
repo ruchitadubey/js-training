859ccafcf9720598a6fe0e4eb3e3252895ca2ca6
ruchitadubey <ruchitadubey19@gmail.com>
2021-04-05 21:38:20 +0530

    loop in array

---
+++ looarray.js
@@ -0,0 +1,21 @@
+// create an aray
+
+const friends =['A','B','C','D'];
+
+const family =['E','F','G','H'];
+
+//FAT ARROW FUNCTION
+
+ family.forEach(y=>{
+
+    console.log(y);
+
+
+ });
+ friends.forEach(ks);
+
+ function ks(x){
+ console.log(x)
+
+ }
+
